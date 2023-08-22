773de00551c34908fb846f1fda5c71dfc684b0b6
conradogui <gui14fbce@gmail.com>
2023-08-21 20:35:03 -0400

    treino de javascript basico

---
+++ sala/logica-de-progamacao/index.js
@@ -0,0 +1,19 @@
+// const nome = 'Guilherme'
+// let valor = '1'
+// console.log(typeof(nome))
+// console.log(typeof "1")
+// console.log(typeof 1)
+// console.log( typeof(true))
+// console.log("1" == 1)
+// console.log( "1" === 1)
+
+// const array = [1, 2, 3, 4, 5, 5, 6, 'Guilherme', 'Conrado']
+// console.log(array)
+const num = 10
+if (num > 0) {
+    console.log('Numero é positivo')
+} else if (num === 0) {
+    console.log('Numero igual a zero')
+}else {
+    console.log(false, 'Numero é negativo')
+}
